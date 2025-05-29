c080d28537583503bb18f5aa5782c9ec0f509022
Miłosz Bochenek <miloszbochenek13@gmail.com>
2025-05-29 12:45:10 +0200

    Event model setup: db migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_05_28_164100) do
+ActiveRecord::Schema[8.0].define(version: 2025_05_29_104056) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
+
+  create_table "events", force: :cascade do |t|
+    t.string "title"
+    t.datetime "date"
+    t.string "location"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
