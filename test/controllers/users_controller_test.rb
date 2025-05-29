5d0944c2d7ff8e987d2cc7f62ab210ff82ccc7af
Miłosz Bochenek <miloszbochenek13@gmail.com>
2025-05-29 20:47:06 +0200

    Adjusting tests sample data v3

--- test/controllers/users_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require "test_helper"
-
-class UsersControllerTest < ActionDispatch::IntegrationTest
-  test "should get show" do
-    sign_in users(:one) # potrzebny helper Devise do testów kontrolerów
-    get user_url(users(:one))
-    assert_response :success
-  end
-end
