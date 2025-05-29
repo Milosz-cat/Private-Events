5d800e575a86ed62bfaeafc1fc7122879e15d814
Miłosz Bochenek <miloszbochenek13@gmail.com>
2025-05-29 21:03:38 +0200

    Creating new events functionality

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,25 @@
 class EventsController < ApplicationController
+  before_action :authenticate_user!
   def index
     @events = Event.all
+  end
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @event = current_user.created_events.build(event_params)
+    if @event.save
+      redirect_to events_path, notice: "Wydarzenie zostało utworzone."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:title, :date, :location)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get "users/show"
   devise_for :users
 
-  resources :events, only: [ :index ]
+  resources :events, only: [ :index, :new, :create ]
   root "events#index"
 
   get "up" => "rails/health#show", as: :rails_health_check
